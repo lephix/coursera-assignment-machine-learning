9e993e60613f4d5beaa30f43b75831047ed8af32
Lephix <lephix@163.com>
2014-07-31 11:43:17 +0800

    finish homework for exercise 3

--- mlclass-ex3-006/mlclass-ex3/predict.m
+++ mlclass-ex3-006/mlclass-ex3/predict.m
@@ -21,15 +21,18 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
-
-
-
-
-
 % =========================================================================
 
+X = [ones(size(X, 1), 1), X];
+a1 = X;
+
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(size(a2,1), 1), a2];
+
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+
+[maxvalue, p] = max(a3, [], 2);
 
 end
